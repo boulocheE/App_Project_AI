b4e17fb85a25dd28c85873aeb2240fb09d346b3e
blc_ele <boulocheeleonore28@gmail.com>
2024-01-19 13:53:18 +0100

    maj page ex & css

--- script.js
+++ script.js
@@ -24,3 +24,29 @@ function getValue() {
 	// Afficher la valeur
 	alert(input);
 }
+
+
+
+function changementEtat ( event, pageActuelle, pageSuivante, menu1, menu2 ) {
+	pageSuivante = document.getElementById( pageSuivante );
+	pageSuivante.style.display = 'block';
+
+	pageActuelle = document.getElementById( pageActuelle );
+	pageActuelle.style.display = 'none';
+
+
+	menu1 = document.getElementById( menu1 );
+	menu1.classList.remove("choix");
+
+	menu2 = document.getElementById( menu2 );
+	menu2.classList.add("choix");
+}
+
+
+function formulaireFichierTexte ( event, choix1, choix2 ) {
+	choix2 = document.getElementById( choix2 );
+	choix2.style.display = 'block';
+
+	choix1 = document.getElementById( choix1 );
+	choix1.style.display = 'none';
+}
