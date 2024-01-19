ef54d4546de84764e7f9b5a01a6fac8cefdafc5a
blc_ele <boulocheeleonore28@gmail.com>
2024-01-19 11:52:56 +0100

    Site maj accueil + css / dev pageEx projet

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ document.addEventListener('DOMContentLoaded', function() {
 	});
 
 });
+
+
+
+function getValue() {
+	// Sélectionner l'élément input et récupérer sa valeur
+	var input = document.getElementById("in").value;
+
+	// Afficher la valeur
+	alert(input);
+}
